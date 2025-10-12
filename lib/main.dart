3eed155ff983ff9f3a7a0cf4195cd388616282b5
Diegoafkx <diego.arreaza@correo.unimet.edu.ve>
2025-10-12 19:50:08 -0400

    Esta la mitad del codigo del front de la ventana donde colocaras el codigo que se envia para recuperar contrasena listo

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
@@ -8,15 +7,67 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Material App',
       home: Scaffold(
+        
+        backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
         appBar: AppBar(
-          title: const Text('Material App Bar'),
+          toolbarHeight: 145,
+          backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
+          leading: Image.asset(
+              'images/Logo.png',
+              width: 164,
+              height: 231
+              ,),
+              leadingWidth: 300,
+          title:
+           const Text(
+            'MetroBox',
+            style: TextStyle(
+              fontSize: 64,
+              color: Color.fromRGBO(240, 83, 43, 1)
+              ),
+            ),
+          centerTitle: true,
         ),
-        body: const Center(
-          child: Text('Hello World'),
+        body: const Column(
+          children: [
+            Divider(
+              color: Color.fromRGBO(65, 64, 64, 1),
+              height: 4,
+              thickness: 10,
+              indent: 0,
+              endIndent: 0,
+            ),
+            Expanded(child: 
+            Center(
+              child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Text(
+                      'Comprueba si has recibido en el correo el código de verificación y escríbelo en el campo correspondiente para continuar con el proceso de registro.',
+                      style: TextStyle(
+                        fontSize: 24,
+                        color: Color.fromRGBO(248, 131, 49, 1)
+
+                      ),
+                    ),
+                    SizedBox(height: 16),
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Código de verificación',
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            ),
+          ],
         ),
       ),
     );
   }
-}
+}  
