b3ae975cd9f5a70305fd0b9ebd56a2e386bafb54
Diegoafkx <diego.arreaza@correo.unimet.edu.ve>
2025-10-19 15:51:51 -0400

    Termine el front de la clase

--- lib/nueva_contrasena.dart
+++ lib/nueva_contrasena.dart
@@ -14,7 +14,11 @@ class NuevaContrasenaScreen extends StatefulWidget {
 
 class _NuevaContrasenaScreenState extends State<NuevaContrasenaScreen> {
   // Aquí va el código del estado (variables, métodos, etc.)
-  
+  final TextEditingController _nuevaContrasenaController = TextEditingController();
+  final TextEditingController _confirmarContrasenaController = TextEditingController();
+  Text advertencia = const Text('Las contraseñas no coinciden.', style: TextStyle(color: Colors.red),);
+  bool visibilidadAdvertencia = false;
+
   @override
   Widget build(BuildContext context) {
     // Aquí va la interfaz de usuario
@@ -50,89 +54,108 @@ class _NuevaContrasenaScreenState extends State<NuevaContrasenaScreen> {
               indent: 0,
               endIndent: 0,
             ),
-            const SizedBox(height: 100),
-            Container(
-                width: 1000.0,
-                margin: const EdgeInsets.all(30),
-                padding: const EdgeInsets.all(20),
-                decoration: BoxDecoration(
-                  color: const Color.fromRGBO(208, 215, 255, 1),
-                  border: Border.all(
-                    color: const Color.fromRGBO(65, 64, 64, 0.95),
-                    width: 4,
+          const SizedBox(height: 100),
+          Container(
+            width: 1000.0,
+            margin: const EdgeInsets.all(30),
+            padding: const EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              color: const Color.fromRGBO(208, 215, 255, 1),
+              border: Border.all(
+                color: const Color.fromRGBO(65, 64, 64, 0.95),
+                width: 4,
+              ),
+              borderRadius: BorderRadius.circular(12),
+            ),
+            child: Column(
+                children: [
+                  const Align(
+                    alignment: Alignment.topLeft,
+                    child: Text(
+                    'Nuevas Contraseña',
+                    style: TextStyle(
+                      fontSize: 50,
+                      color: Color.fromRGBO(240, 83, 43, 1),
+                      fontWeight: FontWeight.bold,
+                      ), 
+                    ), 
                   ),
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Column(
+                  
+                  const Divider(
+                    color: Color.fromRGBO(65, 64, 64, 95),
+                    height: 4,
+                    thickness: 4,
+                    indent: 0,
+                    endIndent: 0,
+                  ),
+                  const SizedBox(height: 20),
+                  TextField(
+                    controller: _nuevaContrasenaController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: 'Nueva contraseña',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 12),
+                  TextField(
+                    controller: _confirmarContrasenaController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                      filled: true,
+                      fillColor: Colors.white,
+                      labelText: 'Confirmar contraseña',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  const SizedBox(height: 20),
+                  Visibility(visible: visibilidadAdvertencia,child: advertencia),
+                  const SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      const Align(
-                        alignment: Alignment.topLeft,
-                        child: Text(
-                        'Nuevas Contraseña',
-                        style: TextStyle(
-                          fontSize: 50,
-                          color: Color.fromRGBO(240, 83, 43, 1),
-                          fontWeight: FontWeight.bold,
-                          ), 
-                        ), 
-                      ),
-                      
-                      const Divider(
-                        color: Color.fromRGBO(65, 64, 64, 95),
-                        height: 4,
-                        thickness: 4,
-                        indent: 0,
-                        endIndent: 0,
-                      ),
-                      const SizedBox(height: 20),
-                      TextField(
-                        controller: _nuevaContrasenaController,
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                          filled: true,
-                          fillColor: Colors.white,
-                          labelText: 'Nueva contraseña',
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                      const SizedBox(height: 12),
-                      TextField(
-                        controller: _confirmarContrasenaController,
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                          filled: true,
-                          fillColor: Colors.white,
-                          labelText: 'Confirmar contraseña',
-                          border: OutlineInputBorder(),
-                        ),
-                      ),
-                      const SizedBox(height: 20),
                       ElevatedButton(
                         style: ElevatedButton.styleFrom(
                           backgroundColor: colorBoton,
                           padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                          )
                         ),
-                        onPressed: _guardarNuevaContrasena,
+                        onPressed: (){},
+                        child: const Text(
+                          'Regresar',
+                          style: TextStyle(fontSize: 18, color: Colors.white),
+                        ),
+                      ),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: colorBoton,
+                          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 12),
+                          shape: const RoundedRectangleBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(10)),
+                          )
+                        ),
+                        onPressed:(){
+                          _guardarNuevaContrasena();
+                        }, 
                         child: const Text(
                           'Guardar contraseña',
                           style: TextStyle(fontSize: 18, color: Colors.white),
                         ),
                       ),
                     ],
-                  ),
-                ),
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
     );
   }
-
-  final _nuevaContrasenaController = TextEditingController();
-  final _confirmarContrasenaController = TextEditingController();
-
-
-  
-  
 
   @override
   void dispose() {
@@ -141,38 +164,48 @@ class _NuevaContrasenaScreenState extends State<NuevaContrasenaScreen> {
     super.dispose();
   }
 
-
   void _guardarNuevaContrasena() {
     final nueva = _nuevaContrasenaController.text;
     final confirmar = _confirmarContrasenaController.text;
 
 
     if (nueva.isEmpty || confirmar.isEmpty) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text('Por favor, completa ambos campos.'),
-          backgroundColor: Colors.orange,
-        ),
+      setState(() {
+        advertencia = const Text('Por favor, completa ambos campos.', style: TextStyle(color: Colors.red),);
+        visibilidadAdvertencia = true;
+        }
       );
       return; 
     }
 
     if (nueva != confirmar) {
-      ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar(
-          content: Text('Las contraseñas no coinciden.'),
-          backgroundColor: Colors.red,
-        ),
-      );
+      setState(() {
+        advertencia = const Text('Las contraseñas no coinciden.', style: TextStyle(color: Colors.red),);
+        visibilidadAdvertencia = true;
+      });
       return; 
     }
     
-    ScaffoldMessenger.of(context).showSnackBar(
-      const SnackBar(
-        content: Text('ontraseña guardada con éxito!'),
-        backgroundColor: Colors.green,
-      ),
-    );
-    Navigator.of(context).pop();
+    for(int i=0; i<nueva.length; i++){
+      if(nueva[i]==' '){
+        setState(() {
+          advertencia = const Text('Contraseña actualizada con éxito.', style: TextStyle(color: Colors.green),);
+          visibilidadAdvertencia = true;
+        });
+        return;
+      }
+      
+      ingresarNuevaContrasena();
+      regresar();
+    }
+    
+  }
+
+  void ingresarNuevaContrasena() {
+    // Lógica para ingresar la nueva contraseña en el firebase
+  }
+
+  void regresar() {
+    // Lógica para regresar a la pantalla de iniciar sesión
   }
 }
